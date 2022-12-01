21ea826c47095534f4b1e1446aa8afa34fa139ef
Seth Grover <mero.mero.guero@gmail.com>
2022-12-01 07:06:42 -0700

    day 1

---
+++ advent-of-code-2022/01/part1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    elves = [x.split('\n') for x in f.read().split("\n\n")]
+
+cals = sorted([sum([int(x) for x in y if x.isdigit()]) for y in elves])
+
+print(max(cals))
+print(sum(cals[-3:]))
