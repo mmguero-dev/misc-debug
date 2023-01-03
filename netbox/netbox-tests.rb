f3522d557b86b150b4b5f1bce3869bcc94c82caf
Seth Grover <mero.mero.guero@gmail.com>
2023-01-03 11:11:07 -0700

    debugging netbox

--- netbox/netbox-tests.rb
+++ netbox/netbox-tests.rb
@@ -6,6 +6,27 @@ require 'lru_redux'
 require 'json'
 require 'faraday'
 
+def collect_values(hashes)
+  # https://stackoverflow.com/q/5490952
+  hashes.reduce({}){ |h, pairs| pairs.each { |k,v| (h[k] ||= []) << v}; h }
+end
+
+def crush(thing)
+  if thing.is_a?(Array)
+    thing.each_with_object([]) do |v, a|
+      v = crush(v)
+      a << v unless [nil, [], {}, "", "Unspecified", "unspecified"].include?(v)
+    end
+  elsif thing.is_a?(Hash)
+    thing.each_with_object({}) do |(k,v), h|
+      v = crush(v)
+      h[k] = v unless [nil, [], {}, "", "Unspecified", "unspecified"].include?(v)
+    end
+  else
+    thing
+  end
+end
+
 NETBOX_PAGE_SIZE=50
 
 opts = Slop.parse do |o|
@@ -29,31 +50,88 @@ devices = Array.new
 
 # retrieve the list VRFs containing IP address prefixes containing the search key
 query = {:contains => opts[:ip], :offset => 0, :limit => NETBOX_PAGE_SIZE}
-while true do
-    tmpPrefixes = nb.get('/api/ipam/prefixes/', query).body.fetch(:results, [])
-    tmpPrefixes.each do |p|
+begin
+  while true do
+    if (prefixes_response = nb.get('/api/ipam/prefixes/', query).body) and prefixes_response.is_a?(Hash) then
+      tmp_prefixes = prefixes_response.fetch(:results, [])
+      tmp_prefixes.each do |p|
         if (vrf = p.fetch(:vrf, nil))
-          vrfs << vrf
+          # non-verbose output is flatter with just names { :name => "name", :id => "id", ... }
+          # if verbose, include entire object as :details
+          vrfs << { :name => vrf.fetch(:name, vrf.fetch(:display, nil)),
+                    :id => vrf.fetch(:id, nil),
+                    :site => ((site = p.fetch(:site, nil)) && site&.key?(:name)) ? site[:name] : site&.fetch(:display, nil),
+                    :tenant => ((tenant = p.fetch(:tenant, nil)) && tenant&.key?(:name)) ? tenant[:name] : tenant&.fetch(:display, nil),
+                    :url => p.fetch(:url, vrf.fetch(:url, nil)),
+                    :details => opts[:verbose] ? vrf.merge({:prefix => p.tap { |h| h.delete(:vrf) }}) : nil }
         end
+      end
+      query[:offset] += tmp_prefixes.length()
+      break unless (tmp_prefixes.length() >= NETBOX_PAGE_SIZE)
+    else
+      break
     end
-    query[:offset] += tmpPrefixes.length()
-    break unless (tmpPrefixes.length() >= NETBOX_PAGE_SIZE)
+  end
+rescue Faraday::Error
+  # give up aka do nothing
 end
+vrfs = collect_values(crush(vrfs))
 
-# retrieve the list IP addresses where address matches the search key, limited to "assigned" addresses.
-# then, for those IP addresses, search for devices pertaining to the interfaces assigned to each
-# IP address (e.g., ipam.ip_address -> dcim.interface -> dcim.device, or
-# ipam.ip_address -> virtualization.interface -> virtualization.virtual_machine)
 query = {:address => opts[:ip], :offset => 0, :limit => NETBOX_PAGE_SIZE}
-while true do
-    tmpIpAddresses = nb.get('/api/ipam/ip-addresses/', query).body.fetch(:results, [])
-    tmpIpAddresses.each do |i|
-        if (obj = i.fetch(:assigned_object, nil)) && ((device = obj.fetch(:device, nil)) || (device = obj.fetch(:virtual_machine, nil)))
-            devices << device
-        end
-    end
-    query[:offset] += tmpIpAddresses.length()
-    break unless (tmpIpAddresses.length() >= NETBOX_PAGE_SIZE)
-end
+begin
+  while true do
+    if (ip_addresses_response = nb.get('/api/ipam/ip-addresses/', query).body) and ip_addresses_response.is_a?(Hash) then
+      tmp_ip_addresses = ip_addresses_response.fetch(:results, [])
+      tmp_ip_addresses.each do |i|
+        is_device = nil
+        if (obj = i.fetch(:assigned_object, nil)) && ((device_obj = obj.fetch(:device, nil)) || (virtualized_obj = obj.fetch(:virtual_machine, nil)))
+          is_device = !device_obj.nil?
+          device = is_device ? device_obj : virtualized_obj
+          # if we can, follow the :assigned_object's "full" device URL to get more information
+          device = (device.key?(:url) and (full_device = nb.get(device[:url]).body)) ? full_device : device
+          device_id = device.fetch(:id, nil)
+          device_site = ((site = device.fetch(:site, nil)) && site&.key?(:name)) ? site[:name] : site&.fetch(:display, nil)
 
-puts JSON.pretty_generate({:vrfs => vrfs.uniq, :devices => devices.uniq})
+          # look up service if requested (based on device/vm found and service port)
+          services = Array.new
+          service_query = { (is_device ? :device_id : :virtual_machine_id) => device_id, :offset => 0, :limit => NETBOX_PAGE_SIZE }
+          while true do
+            if (services_response = nb.get('/api/ipam/services/', service_query).body) and services_response.is_a?(Hash) then
+              tmp_services = services_response.fetch(:results, [])
+              services.unshift(*tmp_services) unless tmp_services.nil? || tmp_services&.empty?
+              service_query[:offset] += tmp_services.length()
+              break unless (tmp_services.length() >= NETBOX_PAGE_SIZE)
+            else
+              break
+            end
+          end
+          device[:service] = services
+
+          # non-verbose output is flatter with just names { :name => "name", :id => "id", ... }
+          # if verbose, include entire object as :details
+          devices << { :name => device.fetch(:name, device.fetch(:display, nil)),
+                       :id => device_id,
+                       :url => device.fetch(:url, nil),
+                       :service => device.fetch(:service, []).map {|s| s.fetch(:name, s.fetch(:display, nil)) },
+                       :site => device_site,
+                       :role => ((role = device.fetch(:role, device.fetch(:device_role, nil))) && role&.key?(:name)) ? role[:name] : role&.fetch(:display, nil),
+                       :cluster => ((cluster = device.fetch(:cluster, nil)) && cluster&.key?(:name)) ? cluster[:name] : cluster&.fetch(:display, nil),
+                       :device_type => ((dtype = device.fetch(:device_type, nil)) && dtype&.key?(:name)) ? dtype[:name] : dtype&.fetch(:display, nil),
+                       :manufacturer => ((manuf = device.dig(:device_type, :manufacturer)) && manuf&.key?(:name)) ? manuf[:name] : manuf&.fetch(:display, nil),
+                       :details => opts[:verbose] ? device : nil }
+          puts(JSON.pretty_generate(:service => device.fetch(:service, []).map {|s| s.fetch(:name, s.fetch(:display, nil)) }))
+        end
+      end
+      query[:offset] += tmp_ip_addresses.length()
+      break unless (tmp_ip_addresses.length() >= NETBOX_PAGE_SIZE)
+    else
+      break
+    end
+  end
+rescue Faraday::Error
+  # give up aka do nothing
+end
+devices = collect_values(crush(devices))
+
+puts JSON.pretty_generate(:service => devices.fetch(:service, []))
+# puts JSON.pretty_generate({:vrfs => vrfs, :devices => devices})
