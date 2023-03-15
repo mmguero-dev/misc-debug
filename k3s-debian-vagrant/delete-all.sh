3fded7963de6a354f8d153b2347b5a66a410c855
Seth Grover <mero.mero.guero@gmail.com>
2023-03-15 13:27:54 -0600

    Work in progress

--- k3s-debian-vagrant/delete-all.sh
+++ k3s-debian-vagrant/delete-all.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 kubectl --kubeconfig shared/k3s.yaml delete -f nginx-ldap/nginx-k3s.yml
-kubectl --kubeconfig shared/k3s.yaml get configmap | awk '{print $1}' | tail -n +2 | xargs -r -l kubectl --kubeconfig shared/k3s.yaml delete configmap
+kubectl --kubeconfig shared/k3s.yaml get configmap --namespace nginx-ldap | awk '{print $1}' | tail -n +2 | grep -v "kube-root-ca\.crt" | xargs -r -l kubectl --kubeconfig shared/k3s.yaml delete configmap
+kubectl --kubeconfig shared/k3s.yaml delete namespace nginx-ldap
--- k3s-debian-vagrant/nginx-startup.sh
+++ k3s-debian-vagrant/nginx-startup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 kubectl --kubeconfig shared/k3s.yaml delete -f nginx-ldap/nginx-k3s.yml
-kubectl --kubeconfig shared/k3s.yaml get configmap | awk '{print $1}' | tail -n +2 | xargs -r -l kubectl --kubeconfig shared/k3s.yaml delete configmap
-kubectl --kubeconfig shared/k3s.yaml create configmap etc-nginx --from-file nginx-ldap/nginx
-kubectl --kubeconfig shared/k3s.yaml create configmap etc-nginx-catrust --from-file nginx-ldap/nginx/ca-trust
-kubectl --kubeconfig shared/k3s.yaml create configmap etc-nginx-certs --from-file nginx-ldap/nginx/certs
-kubectl --kubeconfig shared/k3s.yaml create configmap etc-nginx-certs-pem --from-file nginx-ldap/nginx/certs/dhparam.pem
+kubectl --kubeconfig shared/k3s.yaml get configmap --namespace nginx-ldap | awk '{print $1}' | tail -n +2 | grep -v "kube-root-ca\.crt" | xargs -r -l kubectl --kubeconfig shared/k3s.yaml delete configmap
+kubectl --kubeconfig shared/k3s.yaml delete namespace nginx-ldap
+kubectl --kubeconfig shared/k3s.yaml create namespace nginx-ldap
+kubectl --kubeconfig shared/k3s.yaml create configmap etc-nginx --from-file nginx-ldap/nginx --namespace nginx-ldap
+kubectl --kubeconfig shared/k3s.yaml create configmap etc-nginx-catrust --from-file nginx-ldap/nginx/ca-trust --namespace nginx-ldap
+kubectl --kubeconfig shared/k3s.yaml create configmap etc-nginx-certs --from-file nginx-ldap/nginx/certs --namespace nginx-ldap
+kubectl --kubeconfig shared/k3s.yaml create configmap etc-nginx-certs-pem --from-file nginx-ldap/nginx/certs/dhparam.pem --namespace nginx-ldap
 kubectl --kubeconfig shared/k3s.yaml apply -f nginx-ldap/nginx-k3s.yml
