6c90d2581a139b3fccc19b5a70cbfa43fa9df64c
SG <malcolm.netsec@gmail.com>
2023-03-16 15:35:38 -0600

    work in progress

--- k3s-debian-vagrant/malcolm-startup.sh
+++ k3s-debian-vagrant/malcolm-startup.sh
@@ -64,13 +64,13 @@ set -e
     --from-file "${MALCOLM_PATH}"/nginx/htpasswd \
     --namespace "${K8S_NAMESPACE}"
 "${KUBECTL_CMD[@]}" create configmap var-local-catrust-volume \
-    --from-file nginx/ca-trust \
+    --from-file "${MALCOLM_PATH}"/nginx/ca-trust \
     --namespace "${K8S_NAMESPACE}"
 "${KUBECTL_CMD[@]}" create configmap etc-nginx-certs \
-    --from-file nginx/certs \
+    --from-file "${MALCOLM_PATH}"/nginx/certs \
     --namespace "${K8S_NAMESPACE}"
 "${KUBECTL_CMD[@]}" create configmap etc-nginx-certs-pem \
-    --from-file nginx-ldap/nginx/certs/dhparam.pem \
+    --from-file "${MALCOLM_PATH}"/nginx-ldap/nginx/certs/dhparam.pem \
     --namespace "${K8S_NAMESPACE}"
 
 set +e
