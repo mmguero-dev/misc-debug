183596f2f07dce255014c05cd6d1db62de48b6d6
Seth Grover <mero.mero.guero@gmail.com>
2023-03-16 07:30:43 -0600

    script tweaks

--- k3s-debian-vagrant/nginx-startup.sh
+++ k3s-debian-vagrant/nginx-startup.sh
@@ -4,10 +4,11 @@ K3S_CFG=shared/k3s.yaml
 K8S_NAMESPACE=nginx-ldap
 GET_OPTIONS=(-o wide)
 KUBECTL_CMD=(kubectl --kubeconfig "${K3S_CFG}")
+NOT_FOUND_REGEX="(No resources|not) found"
 
-"${KUBECTL_CMD[@]}" delete -f nginx-ldap/nginx-k3s.yml
-"${KUBECTL_CMD[@]}" get configmap --namespace "${K8S_NAMESPACE}" | awk '{print $1}' | tail -n +2 | grep -v "kube-root-ca\.crt" | xargs -r -l "${KUBECTL_CMD[@]}" delete configmap
-"${KUBECTL_CMD[@]}" delete namespace "${K8S_NAMESPACE}"
+"${KUBECTL_CMD[@]}" delete -f nginx-ldap/nginx-k3s.yml 2>&1 | grep -Piv "${NOT_FOUND_REGEX}"
+"${KUBECTL_CMD[@]}" get configmap --namespace "${K8S_NAMESPACE}" 2>/dev/null | awk '{print $1}' | tail -n +2 | grep -v "kube-root-ca\.crt" | xargs -r -l "${KUBECTL_CMD[@]}" delete configmap 2>&1 | grep -Piv "not found"
+"${KUBECTL_CMD[@]}" delete namespace "${K8S_NAMESPACE}" 2>&1 | grep -Piv "${NOT_FOUND_REGEX}"
 "${KUBECTL_CMD[@]}" create namespace "${K8S_NAMESPACE}"
 "${KUBECTL_CMD[@]}" create configmap etc-nginx --from-file nginx-ldap/nginx --namespace "${K8S_NAMESPACE}"
 "${KUBECTL_CMD[@]}" create configmap etc-nginx-catrust --from-file nginx-ldap/nginx/ca-trust --namespace "${K8S_NAMESPACE}"
@@ -28,6 +29,6 @@ for ITEM in \
     volumeattachments;
 do
     echo "------ ${ITEM}"
-    "${KUBECTL_CMD[@]}" "${GET_OPTIONS[@]}" get "${ITEM}" --namespace "${K8S_NAMESPACE}" 2>&1 | grep -v "No resources"
+    "${KUBECTL_CMD[@]}" "${GET_OPTIONS[@]}" get "${ITEM}" --namespace "${K8S_NAMESPACE}" 2>&1 | grep -Piv "${NOT_FOUND_REGEX}"
 done
 echo
