f1e507134cc3aad4e9df169e603e41d7f7496f72
Seth Grover <mero.mero.guero@gmail.com>
2023-06-19 10:35:32 -0600

    POC with mimic

--- mimictts/generate_voice_oggs.sh
+++ mimictts/generate_voice_oggs.sh
@@ -9,17 +9,30 @@ for VOICE in $(mimic3 --voices | grep ^en | awk '{print $1}'); do
         tee "${OUTFILE}.ssml" >/dev/null <<EOF
 <speak>
   <voice name="${VOICE}#${SPEAKER}">
-    <s>
-      ${TEST_STRING}
-    </s>
+    Ozymandias
     <break time="1s"/>
-    <s>
-      This voice was ${VOICE} with the speaker ${SPEAKER}.
-    </s>
+    by Percy Bysshe Shelley.
+    <break time="2s"/>
+    I met a traveller from an antique land,
+    Who said, "Two vast and trunkless legs of stone
+    Stand in the desert. . . . Near them, on the sand,
+    Half sunk a shattered visage lies, whose frown,
+    And wrinkled lip, and sneer of cold command,
+    Tell that its sculptor well those passions read
+    Which yet survive, stamped on these lifeless things,
+    The hand that mocked them, and the heart that fed;
+    And on the pedestal, these words appear:
+    My name is Ozymandias, King of Kings;
+    Look on my Works, ye Mighty, and despair!
+    Nothing beside remains. Round the decay
+    Of that colossal Wreck, boundless and bare
+    The lone and level sands stretch far away."
+    <break time="5s"/>
+    This voice was ${VOICE} with the speaker ${SPEAKER}.
   </voice>
 </speak>
 EOF
-        mimic3 --ssml >"${OUTFILE}.wav" 2>/dev/null <"${OUTFILE}.ssml" && \
+        mimic3 --ssml >"${OUTFILE}.wav" <"${OUTFILE}.ssml" 2>/dev/null && \
             ffmpeg -hide_banner -loglevel error -y -i "${OUTFILE}.wav" -c:a libvorbis -qscale:a 5 -ar 44100 -ac 1 "${OUTFILE}.ogg" && \
             rm -f "${OUTFILE}.wav" "${OUTFILE}.ssml"
     done
